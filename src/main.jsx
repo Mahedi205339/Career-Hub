67a0c01ae786faf72e49d21278886116bfb9a909
mahedi205339 <mahedi205339gmaol.com>
2023-09-24 14:09:27 +0600

    added header footer in components  folder

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from './components/root/root';
+import Home from './components/Home/Home';
+import Appliedjobs from './components/Appliedjobs/Appliedjobs';
+import ErrorPage from './components/Errorpege/ErrorPage';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    errorElement:<ErrorPage></ErrorPage>,
+    children : [
+      {
+        path:'/',
+        element: <Home></Home>
+      },
+      {
+        path:'/applied',
+        element: <Appliedjobs></Appliedjobs>
+      }
+    ]
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
